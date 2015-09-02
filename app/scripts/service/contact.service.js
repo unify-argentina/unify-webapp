4a30a073bdf7cb1a102d6510e731d32ab865fe26
Juan Cistaro <juan.cistaro@graion.com>
2015-09-01 21:48:29 -0300

    correct contact

--- app/scripts/service/contact.service.js
+++ app/scripts/service/contact.service.js
@@ -15,11 +15,7 @@ unifyApp.factory('ContactService', 	function($http, $resource, ENV) {
 					user_id : contact.user_id,
 					name : contact.name,
 					picture : contact.picture,
-<<<<<<< HEAD
-					circles_ids : contact.circles_ids,
-=======
 					circles_ids : [contact.circle_id],
->>>>>>> dc7d75f9969449e27c1463437c58a5cae92588ec
 					facebook_id : (contact.facebook!=null?contact.facebook.id:null),
 					twitter_id : (contact.twitter!=null?contact.twitter.id:null),
 					instagram_id : (contact.instagram!=null?contact.instagram.id:null),
