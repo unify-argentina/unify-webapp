660473cef1be2885f6892f34b59debbd1bbc89e8
Juan Cistaro <juan.cistaro@graion.com>
2015-09-24 09:27:23 -0300

    typo en profile edit, link a contacto desde timeline

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -1,7 +1,13 @@
 unifyApp.directive('uwTimeLine', function() {
+  
+  	var link = function(scope, elm, attrs, ctrl) {
 
+       
+
+    };
     return {
         restrict: 'E',
+        link: link,
         templateUrl: 'views/timeline.html',
         scope:{
         	uwMedia: "=",
