9aad0ec7b59b56d0fc0c049b45ff02270c6ef37a
Juan Cistaro <juan.cistaro@graion.com>
2015-09-29 20:43:47 -0300

    links hashtag

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -1,7 +1,7 @@
 unifyApp.directive('uwTimeLine', function($sce, $filter) {
   
   	var link = function(scope, elm, attrs, ctrl) {
-        scope.boldHashtags = function(text){
+        scope.boldHashtags = function(text, provider){
 
             var target = "_blank";
             var replacedText = $filter('linky')(text, target);
@@ -9,10 +9,20 @@ unifyApp.directive('uwTimeLine', function($sce, $filter) {
             if (angular.isDefined(target)) {
                 targetAttr = ' target="' + target + '"';
             }
-            var replacePattern1 = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
-            replacedText = replacedText.replace(replacePattern1, '$1<b class="hashtag">#$2</b>');
-            var replacePattern2 = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
-            replacedText = replacedText.replace(replacePattern2, '$1<b class="mention">@$2</b>');
+            if(provider=='twitter'){
+                var replacePattern1 = /(^|\s)#(\w*[ñÑáÁäÄâÂàÀéÉëËêÊèÈíÍïÏîÎìÌóÓöÖôÔòÒúÚüÜûÛùÙ\.\-a-zA-Z_]+\w*)/igm;
+                replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://twitter.com/search?q=%23$2"' + targetAttr + '>#$2</a>');
+                var replacePattern2 = /(^|\s)@(\w*[ñÑáÁäÄâÂàÀéÉëËêÊèÈíÍïÏîÎìÌóÓöÖôÔòÒúÚüÜûÛùÙ\.\-a-zA-Z_]+\w*)/igm;
+                replacedText = replacedText.replace(replacePattern2, '$1<a class="hashtag" href="https://twitter.com/$2"' + targetAttr + '>@$2</a>');
+            }else if(provider == 'facebook'){
+                var replacePattern1 = /(^|\s)#(\w*[ñÑáÁäÄâÂàÀéÉëËêÊèÈíÍïÏîÎìÌóÓöÖôÔòÒúÚüÜûÛùÙ\.\-a-zA-Z_]+\w*)/igm;
+                replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://facebook.com/hashtag/$2"' + targetAttr + '>#$2</a>');
+            }else if(provider == 'instagram'){
+                var replacePattern1 = /(^|\s)#(\w*[ñÑáÁäÄâÂàÀéÉëËêÊèÈíÍïÏîÎìÌóÓöÖôÔòÒúÚüÜûÛùÙ\.\-a-zA-Z_]+\w*)/igm;
+                replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://instagram.com/explore/tags/$2"' + targetAttr + '>#$2</a>');
+                 var replacePattern2 = /(^|\s)@(\w*[ñÑáÁäÄâÂàÀéÉëËêÊèÈíÍïÏîÎìÌóÓöÖôÔòÒúÚüÜûÛùÙ\.\-a-zA-Z_]+\w*)/igm;
+                replacedText = replacedText.replace(replacePattern2, '$1<a class="hashtag" href="https://instagram.com/$2"' + targetAttr + '>@$2</a>');
+            }
             return replacedText;
         }
     };
