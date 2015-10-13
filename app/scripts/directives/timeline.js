8d6f2f09bd0a9baa0d4bd81c9978e127fa610aa1
Juan Cistaro <juan.cistaro@graion.com>
2015-10-13 12:41:25 -0300

    hashtags, mentions

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -10,17 +10,17 @@ unifyApp.directive('uwTimeLine', function($sce, $filter, $modal, ContactService,
             }
             replacedText = replacedText.replace("&#10;", '<br/> ');
             if(provider=='twitter'){
-                var replacePattern1 = /(^|\s|.)#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
+                var replacePattern1 = /(^|\s|[^&])#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
                 replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://twitter.com/search?q=%23$2"' + targetAttr + '>#$2</a>');
-                var replacePattern2 = /(^|\s|.)@(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
+                var replacePattern2 = /(^|\s|[^&])@(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
                 replacedText = replacedText.replace(replacePattern2, '$1<a class="mention" href="https://twitter.com/$2"' + targetAttr + '>@$2</a>');
             }else if(provider == 'facebook'){
-                var replacePattern1 = /(^|\s|.)#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
+                var replacePattern1 = /(^|\s|[^&])#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
                 replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://facebook.com/hashtag/$2"' + targetAttr + '>#$2</a>');
             }else if(provider == 'instagram'){
-                var replacePattern1 = /(^|\s|.)#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
+                var replacePattern1 = /(^|\s|[^&])#(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
                 replacedText = replacedText.replace(replacePattern1, '$1<a class="hashtag" href="https://instagram.com/explore/tags/$2"' + targetAttr + '>#$2</a>');
-                 var replacePattern2 = /(^|\s|.)@(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
+                 var replacePattern2 = /(^|\s|[^&])@(((\w*[\.\-a-zA-Z_]+\w*)|(&#(19?[2-9]|2?[0-4][0-9]|25[0-5]);))*)/igm;
                 replacedText = replacedText.replace(replacePattern2, '$1<a class="mention" href="https://instagram.com/$2"' + targetAttr + '>@$2</a>');
             }
             return replacedText;
