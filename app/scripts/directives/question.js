7626abaeb2ba944ade07a093ccad672a855d0845
Juan Cistaro <juan.cistaro@graion.com>
2015-10-14 16:52:12 -0300

    tutoriales?

--- app/scripts/directives/question.js
+++ app/scripts/directives/question.js
@@ -1,7 +1,25 @@
-unifyApp.directive('uwQuestion', function() {
+unifyApp.directive('uwQuestion', function($rootScope, $timeout) {
 
 	var link = function(scope, elm, attrs, ctrl) {
-        
+        scope.showTutorialText = function(){
+            scope.showDescription=true; 
+            $rootScope.hideQuestionMarks=true;
+            $timeout(function() {
+                scope.showDescription=false; 
+                $rootScope.showQuestions=false;
+            }, 10000);
+        };
+
+        $rootScope.$watch('showQuestions', function(newValue, oldValue) {
+            if(newValue){
+                $timeout(function() {
+                    if(!$rootScope.hideQuestionMarks){
+                        $rootScope.showQuestions=false;
+                    }
+                }, 6000);
+               
+            }
+        });
     };
 
     return {
